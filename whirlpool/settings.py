5c6a2d0a1e84dc841cc6a510e161b1ade6fc9fd4
blaze404 <mus.qazi999@gmail.com>
2022-08-15 14:40:57 +0530

    mysql

--- whirlpool/settings.py
+++ whirlpool/settings.py
@@ -80,13 +80,22 @@ CORS_ALLOW_HEADERS = ['*']
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'whirlpool',
+        'USER': 'djangouser',
+        'PASSWORD': 'devyani'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
