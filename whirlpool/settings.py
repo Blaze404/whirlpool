204ea7c82974639efbf1744d0471ad70339ac164
blaze404 <mus.qazi999@gmail.com>
2022-08-15 12:35:58 +0530

    ssl

--- whirlpool/settings.py
+++ whirlpool/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website.apps.WebsiteConfig',
-    'corsheaders'
+    'corsheaders',
+    "sslserver",
+
 ]
 
 MIDDLEWARE = [
